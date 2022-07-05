8819b342f2691003b5df1149a6b554933a22ce67
KDS <dasomm57@naver.com>
2022-07-05 20:44:22 +0900

    수정4

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -17,6 +17,7 @@ $(function(){
 
   $('.sc-visual .title').click(function (e) {
     e.preventDefault();
+    /* $(this).toggleClass('on') */
     $(this).parent().addClass('active').siblings('.slide').removeClass('active');
   })
 
